ec78a692083ff48e9aefd8880f7381be3925fa96
Rapha Taylor <raphataylor1@gmail.com>
2024-08-09 23:29:59 +0100

    updated data set, separated settings and spaces into 2 jsons. favicons are missing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Global variables
+// global variables
 var width = window.innerWidth,
     height = window.innerHeight;
 
@@ -13,130 +13,157 @@ var svg = d3.select("#graph").append("svg")
 
 var container = svg.append("g");
 
-// Load and process data
-d3.json("space1.json", function(error, data) {
-    if (error) throw error;
-    
-    var space = data.spaces[0];
-    var nodes = space.nodes;
-    var links = space.links;
+// load graph settings
+d3.json("graph-settings.json", function(error, settings) {
+    if (error) {
+        console.error("error loading the graph settings:", error);
+        return;
+    }
 
-    // Create a map of node id to node object
-    var nodeMap = {};
-    nodes.forEach(function(node) {
-        nodeMap[node.id] = node;
-    });
+    // use settings in your code
+    var nodeSize = settings.nodeSize;
+    var linkSize = settings.linkSize;
+    var charge = settings.forceSettings.charge;
+    var linkDistance = settings.forceSettings.linkDistance;
 
-    // Process links to use node objects instead of ids
-    links = links.map(function(link) {
-        return {
-            source: nodeMap[link.source],
-            target: nodeMap[link.target]
-        };
-    });
+    // load and process data
+    d3.json("bookmarks.json", function(error, data) {
+        if (error) {
+            console.error("error loading the bookmarks data:", error);
+            return;
+        }
 
-    // Create groups (one for each tag)
-    var groups = nodes.filter(function(n) { return n.type === "tag"; })
-        .map(function(tag) {
+        var tags = data.tags;
+        var sites = data.sites;
+
+        // create nodes array
+        var nodes = tags.concat(sites);
+
+        // create links array
+        var links = [];
+        sites.forEach(function(site) {
+            site.tags.forEach(function(tagId) {
+                links.push({
+                    source: site.id,
+                    target: tagId
+                });
+            });
+        });
+
+        // create a map of node id to node object
+        var nodeMap = {};
+        nodes.forEach(function(node) {
+            nodeMap[node.id] = node;
+        });
+
+        // process links to use node objects instead of ids
+        links = links.map(function(link) {
+            return {
+                source: nodeMap[link.source],
+                target: nodeMap[link.target]
+            };
+        });
+
+        // create groups (one for each tag)
+        var groups = tags.map(function(tag) {
             return {
                 id: tag.id,
-                nodes: [tag].concat(nodes.filter(function(n) {
-                    return n.type === "site" && links.some(function(l) {
-                        return (l.source.id === tag.id && l.target.id === n.id) || 
-                               (l.target.id === tag.id && l.source.id === n.id);
-                    });
+                nodes: [tag].concat(sites.filter(function(site) {
+                    return site.tags.includes(tag.id);
                 }))
             };
         });
 
-    // Create force simulation
-    var simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(function(d) { return d.id; }).distance(50))
-        .force("charge", d3.forceManyBody().strength(-200))
-        .force("center", d3.forceCenter(width / 2, height / 2))
-        .force("collision", d3.forceCollide().radius(30))
-        .on("tick", ticked);
+        // create force simulation
+        var simulation = d3.forceSimulation(nodes)
+            .force("link", d3.forceLink(links).id(function(d) { return d.id; }).distance(linkDistance))
+            .force("charge", d3.forceManyBody().strength(charge))
+            .force("center", d3.forceCenter(width / 2, height / 2))
+            .force("collision", d3.forceCollide().radius(nodeSize * 3))
+            .on("tick", ticked);
 
-    // Create group backgrounds
-    var group = container.selectAll('.group')
-        .data(groups)
-        .enter().append('path')
-        .attr('class', 'group')
-        .style("fill", function (d, i) { return color(i); })
-        .style("stroke", function (d, i) { return d3.rgb(color(i)).darker(); })
-        .style("opacity", 0.3);
+        // create group backgrounds
+        var group = container.selectAll('.group')
+            .data(groups)
+            .enter().append('path')
+            .attr('class', 'group')
+            .style("fill", function (d, i) { return color(i); })
+            .style("stroke", function (d, i) { return d3.rgb(color(i)).darker(); })
+            .style("opacity", 0.3);
 
-    // Create links
-    var link = container.selectAll(".link")
-        .data(links)
-        .enter().append("line")
-        .attr("class", "link");
+        // create links
+        var link = container.selectAll(".link")
+            .data(links)
+            .enter().append("line")
+            .attr("class", "link")
+            .attr("stroke-width", linkSize);
 
-    // Create nodes
-    var node = container.selectAll(".node")
-        .data(nodes)
-        .enter().append("g")
-        .attr("class", function(d) { return "node " + d.type; })
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
+        // create nodes
+        var node = container.selectAll(".node")
+            .data(nodes)
+            .enter().append("g")
+            .attr("class", function(d) { return "node " + (d.tags ? "site" : "tag"); })
+            .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended));
 
-    // Add circles to nodes
-    node.append("circle")
-        .attr("r", function(d) { return d.type === "tag" ? 20 : 10; });
+        // add circles to nodes
+        node.append("circle")
+            .attr("r", function(d) { return d.tags ? nodeSize : nodeSize * 2; });
 
-    // Add labels to nodes
-    node.append("text")
-        .attr("dy", ".35em")
-        .attr("x", function(d) { return d.type === "tag" ? 25 : 15; })
-        .text(function(d) { return d.name || d.title || d.id; });
+        // add labels to nodes
+        node.append("text")
+            .attr("dy", ".35em")
+            .attr("x", function(d) { return d.tags ? nodeSize * 1.5 : nodeSize * 2.5; })
+            .text(function(d) { return d.name || d.title; });
 
-    // Add favicon to site nodes
-    node.filter(function(d) { return d.type === "site" && d.favicon; })
-        .append("image")
-        .attr("xlink:href", function(d) { return d.favicon; })
-        .attr("x", -8)
-        .attr("y", -8)
-        .attr("width", 16)
-        .attr("height", 16);
+        // add favicon to site nodes (if available)
+        node.filter(function(d) { return d.tags && d.favicon; })
+            .append("image")
+            .attr("xlink:href", function(d) { return d.favicon; })
+            .attr("x", -nodeSize * 0.8)
+            .attr("y", -nodeSize * 0.8)
+            .attr("width", nodeSize * 1.6)
+            .attr("height", nodeSize * 1.6);
 
-    function ticked() {
-        link
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+        function ticked() {
+            link
+                .attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
 
-        node
-            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+            node
+                .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-        group.attr("d", groupPath);
-    }
-
-    function dragstarted(d) {
-        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-        d.fx = d.x;
-        d.fy = d.y;
-    }
-
-    function dragged(d) {
-        d.fx = d3.event.x;
-        d.fy = d3.event.y;
-    }
-
-    function dragended(d) {
-        if (!d3.event.active) simulation.alphaTarget(0);
-        d.fx = null;
-        d.fy = null;
-    }
-
-    function groupPath(d) {
-        var hull = d3.polygonHull(d.nodes.map(function(n) { return [n.x, n.y]; }));
-        if (hull) {
-            return "M" + hull.join("L") + "Z";
-        } else {
-            return "";
+            group.attr("d", groupPath);
         }
-    }
+
+        function dragstarted(d) {
+            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+        }
+
+        function dragged(d) {
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+        }
+
+        function dragended(d) {
+            if (!d3.event.active) simulation.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+        }
+
+        function groupPath(d) {
+            var hull = d3.polygonHull(d.nodes.map(function(n) { return [n.x, n.y]; }));
+            if (hull) {
+                return "M" + hull.join("L") + "Z";
+            } else {
+                return "";
+            }
+        }
+    });
 });
