608bb4e0d048c102cd05b9ef6fec5c252fa13ab5
Rapha Taylor <raphataylor1@gmail.com>
2024-08-16 20:51:45 +0100

    fresh start

--- build/init.js
+++
@@ -1,9 +0,0 @@
-console.log("Index.html loaded");
-window.addEventListener("load", function() {
-    console.log("Window loaded");
-    if (document.getElementById('root').children.length === 0) {
-        console.error("React app not rendered");
-    } else {
-        console.log("React app rendered");
-    }
-});
