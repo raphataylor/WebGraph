e23f631efc8cc9d52f594311470509d08cae466b
Rapha Taylor <raphataylor1@gmail.com>
2024-08-25 12:41:08 +0100

    Reduced unnecessary permissions

--- background.js
+++ background.js
@@ -2,20 +2,6 @@ import DataManager from './utils/DataManager.js';
 
 const dataManager = new DataManager();
 
-function createContextMenu() {
-  chrome.contextMenus.create({
-    id: "openVisualization",
-    title: "Open WebGraph Visualization",
-    contexts: ["all"]
-  }, () => {
-    if (chrome.runtime.lastError) {
-      console.log("Context menu item already exists");
-    } else {
-      console.log("Context menu item created successfully");
-    }
-  });
-}
-
 chrome.runtime.onInstalled.addListener(async (details) => {
   console.log('WebGraph extension installed');
   
@@ -29,17 +15,6 @@ chrome.runtime.onInstalled.addListener(async (details) => {
   } else {
     // For updates or other cases, load existing data
     await dataManager.loadData();
-  }
-
-  // Remove existing context menu items
-  chrome.contextMenus.removeAll(() => {
-    createContextMenu();
-  });
-});
-
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "openVisualization") {
-    chrome.tabs.create({ url: "visualization/GraphVisualization.html" });
   }
 });
 
